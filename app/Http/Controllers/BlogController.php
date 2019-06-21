a1b4cb98b5483eb002f969d59f566b7fc6a0f454
Marlon <717900268@qq.com>
2019-06-21 10:56:26 +0800

    备份

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::where('published_at', '<=', Carbon::now())
+            ->orderBy('published_at', 'desc')
+            ->paginate(config('blog.posts_per_page'));
+        return view('blog.index', compact('posts'));
+    }
+
+    public function showPost($slug)
+    {
+
+        $posts = Post::where('slug', $slug)->firstOrFail();
+        return view('blog.post',['post'=>$posts]);
+    }
+}
